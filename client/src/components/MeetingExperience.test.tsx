d28f8229b20564d4025de4709ff87f2cd3012b4d
ziaflow <jeremy@ziaflow.com>
2025-10-31 02:35:02 -0700

    Initialize release/1.0.12

---
+++ client/src/components/MeetingExperience.test.tsx
@@ -0,0 +1,119 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { CallWithChatComposite } from '@azure/communication-react';
+import { setIconOptions } from '@fluentui/react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { MeetingExperience, MeetingExperienceProps } from './MeetingExperience';
+import * as GetTeamsMeetingLink from '../utils/GetTeamsMeetingLink';
+import {
+  createMockCallWithChatAdapter,
+  createMockCallWithChatComposite,
+  createMockStatefulCallClient,
+  createMockStatefulChatClient,
+  runFakeTimers
+} from '../utils/TestUtils';
+
+configure({ adapter: new Adapter() });
+
+// Disable icon warnings for tests as we don't register the icons for unit tests which causes warnings.
+// See: https://github.com/microsoft/fluentui/wiki/Using-icons#test-scenarios
+setIconOptions({
+  disableWarnings: true
+});
+
+jest.mock('@azure/communication-react', () => {
+  return {
+    ...jest.requireActual('@azure/communication-react'),
+    createAzureCommunicationCallWithChatAdapterFromClients: () => createMockCallWithChatAdapter(),
+    createStatefulCallClient: () => createMockStatefulCallClient(),
+    createStatefulChatClient: () => createMockStatefulChatClient(),
+    CallWithChatComposite: () => createMockCallWithChatComposite()
+  };
+});
+
+jest.mock('@azure/communication-common', () => {
+  return {
+    AzureCommunicationTokenCredential: function () {
+      return { token: '', getToken: () => '' };
+    }
+  };
+});
+
+describe('MeetingExperience', () => {
+  const waitingTitle = 'waiting title';
+  const waitingSubtitle = 'waiting subtitle';
+  const logoUrl = 'logoUrl';
+  let userAgentGetter: any = undefined;
+
+  beforeEach(() => {
+    userAgentGetter = jest.spyOn(window.navigator, 'userAgent', 'get');
+    jest.spyOn(console, 'error').mockImplementation();
+    const getChatThreadIdFromTeamsLinkSpy = jest.spyOn(GetTeamsMeetingLink, 'getChatThreadIdFromTeamsLink');
+    getChatThreadIdFromTeamsLinkSpy.mockReturnValue('threadId');
+  });
+
+  it('should pass props for customizing the lobby experience to the CallWithChatComposite', async () => {
+    const meetingExperience = await mount<MeetingExperienceProps>(
+      <MeetingExperience
+        userId={{ communicationUserId: 'test' }}
+        token={'token'}
+        displayName={'name'}
+        endpointUrl={'endpoint'}
+        locator={{ meetingLink: 'meeting link' }}
+        fluentTheme={undefined}
+        waitingTitle={waitingTitle}
+        waitingSubtitle={waitingSubtitle}
+        logoUrl={logoUrl}
+        chatEnabled={true}
+        onDisplayError={jest.fn()}
+      />
+    );
+
+    await runFakeTimers();
+    meetingExperience.update();
+    const callWithChatComposites = meetingExperience.find(CallWithChatComposite);
+
+    expect(callWithChatComposites.length).toBe(1);
+    expect(callWithChatComposites.first().props().locale?.strings.call.lobbyScreenWaitingToBeAdmittedTitle).toBe(
+      waitingTitle
+    );
+    expect(callWithChatComposites.first().props().locale?.strings.call.lobbyScreenWaitingToBeAdmittedMoreDetails).toBe(
+      waitingSubtitle
+    );
+    expect(callWithChatComposites.first().props().icons?.LobbyScreenConnectingToCall).toBeDefined();
+    expect(callWithChatComposites.first().props().icons?.LobbyScreenWaitingToBeAdmitted).toBeDefined();
+    expect(callWithChatComposites.first().props().formFactor).toEqual('desktop');
+  });
+
+  it('sets the formFactor to mobile in the CallWithChatComposite', async () => {
+    const mobileSafariUserAgent =
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1';
+    userAgentGetter.mockReturnValue(mobileSafariUserAgent);
+
+    const meetingExperience = await mount<MeetingExperienceProps>(
+      <MeetingExperience
+        userId={{ communicationUserId: 'test' }}
+        token={'token'}
+        displayName={'name'}
+        endpointUrl={'endpoint'}
+        locator={{ meetingLink: 'meeting link' }}
+        fluentTheme={undefined}
+        waitingTitle={waitingTitle}
+        waitingSubtitle={waitingSubtitle}
+        logoUrl={logoUrl}
+        chatEnabled={true}
+        onDisplayError={jest.fn()}
+      />
+    );
+
+    await runFakeTimers();
+    meetingExperience.update();
+
+    const callWithChatComposites = meetingExperience.find(CallWithChatComposite);
+
+    expect(callWithChatComposites.length).toBe(1);
+    expect(callWithChatComposites.first().props().formFactor).toEqual('mobile');
+  });
+});
